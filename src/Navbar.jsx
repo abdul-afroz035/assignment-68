fef23f486021903fede232de96f5d33536008c2d
abdul-afroz035 <abdulafroz035@gmail.com>
2025-12-26 15:51:09 +0530

    navbaaar update

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -9,7 +9,7 @@ function Navbar({productCount}) {
         <img class="h-16 object-cover" src="https://upload.wikimedia.org/wikipedia/commons/0/06/Amazon_2024.svg" alt="amazon-logo"/>
         <div className="flex flex-col items-center"> 
           <CiShoppingCart class="text-5xl text-primary-default"/>
-          <span className="-m-7 text-primary-default">{productCount}</span>
+          <span className="-m-9 text-primary-default">{productCount}</span>
         </div>
       </div>
      
